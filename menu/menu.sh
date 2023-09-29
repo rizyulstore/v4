5ecc3f2a4b73f0ba320ea24f15824f543b6ed73d
rizyulstore <127845358+rizyulstore@users.noreply.github.com>
2023-09-29 23:31:55 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -213,7 +213,7 @@ clear
 echo -e ""
 echo -e ""
 echo -e "${BIWhite}  ───────────────────────────────────────────────────── ${NC}"
-echo -e "${NC} ${RED_BG}                ${BIWhite}✙ RIZYULTUNNEL ✙               ${NC}"
+echo -e "${NC} ${RED_BG}                ${BIWhite}✙ RIZYULTUNNEL ✙                  ${NC}"
 echo -e "${BIWhite}  ───────────────────────────────────────────────────── ${NC}"
 echo -e "${BIWhite} ┌─────────────────────────────────────────────────────┐${NC}"
 echo -e "${BIWhite} │                    ${BIWhite}${UWhite}INFORMATIONS${NC}"
