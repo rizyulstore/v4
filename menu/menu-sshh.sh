7bed23fbd6a7d024bb4127f38fe92cdf0231741d
rizyulstore <127845358+rizyulstore@users.noreply.github.com>
2023-09-30 16:41:09 +0700

    Update menu-sshh.sh

--- menu/menu-sshh.sh
+++ menu/menu-sshh.sh
@@ -55,11 +55,63 @@ NC='\e[0m'
 yelow="\e[1;33m"
 white="\e[1;37m"
 cyann="\e[1;36m"
+# // Exporting Language to UTF-8
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+
+# // Exporting Language to UTF-8
+
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US.UTF-8'
+
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+export RED_BG="\e[50m"
 
 green_background="\033[42;37m"
 red_background="\033[41;37m"
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
 PERMISSION
 if [ -f /home/needupdate ]; then
 red "Your script need to update first !"
@@ -72,22 +124,22 @@ exit 0
 fi
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
-echo -e " ${BIWhite}┌─────────────────────────────────────────────────────┐${NC}"
-echo -e " ${BIWhite}│                  ${red_background}SSH MENU                   │${NC}" 
-echo -e " ${BIWhite}│==============================================================│"
-echo -e " ${BIWhite}│       ${white}[•1]${NC} ${cyann}Create ssh${NC}"
-echo -e "        ${white}[•2]${NC} ${cyann}Create trial${NC}"
-echo -e "        ${white}[•3]${NC} ${cyann}Renew ssh${NC}"
-echo -e "        ${white}[•4]${NC} ${cyann}Delet ssh${NC}"
-echo -e "        ${white}[•5]${NC} ${cyann}Cek user login${NC}"
-echo -e "        ${white}[•6]${NC} ${cyann}List user${NC}"
-echo -e "        ${white}[•7]${NC} ${cyann}Delet ssh expired${NC}"
-echo -e "        ${white}[•8]${NC} ${cyann}Auto Kill ssh${NC}"
-echo -e "        ${white}[•9]${NC} ${cyann}Cek multilogin ssh${NC}"
-echo -e "        ${white}[•10]${NC} ${cyann}user lock${NC}"
-echo -e "        ${white}[•11]${NC} ${cyann}user unlock${NC}"
-echo -e "        ${yelow}[•0]${NC} ${yelow}Back To Menu${NC}"
-echo -e " ${BIWhite}└─────────────────────────────────────────────────────┘${NC}"
+echo -e " ${BIWhite}┌───────────────────────────────────────────────┐${NC}"
+echo -e " ${BIWhite}│                ${BIWhite}  SSH MENU                   │${NC}" 
+echo -e " ${BIWhite}│=======================================================│"
+echo -e " ${BIWhite}│        ${white}[•1]${NC} ${cyann}Create ssh${NC}"
+echo -e " ${BIWhite}│        ${white}[•2]${NC} ${cyann}Create trial${NC}"
+echo -e " ${BIWhite}│        ${white}[•3]${NC} ${cyann}Renew ssh${NC}"
+echo -e " ${BIWhite}│        ${white}[•4]${NC} ${cyann}Delet ssh${NC}"
+echo -e " ${BIWhite}│        ${white}[•5]${NC} ${cyann}Cek user login${NC}"
+echo -e " ${BIWhite}│        ${white}[•6]${NC} ${cyann}List user${NC}"
+echo -e " ${BIWhite}│        ${white}[•7]${NC} ${cyann}Delet ssh expired${NC}"
+echo -e " ${BIWhite}│        ${white}[•8]${NC} ${cyann}Auto Kill ssh${NC}"
+echo -e " ${BIWhite}│        ${white}[•9]${NC} ${cyann}Cek multilogin ssh${NC}"
+echo -e " ${BIWhite}│        ${white}[•10]${NC} ${cyann}user lock${NC}"
+echo -e " ${BIWhite}│        ${white}[•11]${NC} ${cyann}user unlock${NC}"
+echo -e " ${BIWhite}│        ${yelow}[•0]${NC} ${yelow}Back To Menu${NC}"
+echo -e " ${BIWhite}└────────────────────────────────────────────────┘${NC}"
 echo -e "${yelow}"
 read -p " >>>  "  opt
 echo -e ""
