e4b80bf99555a1d693eea1cecbcc3df7194cc09f
rizyulstore <127845358+rizyulstore@users.noreply.github.com>
2023-10-03 17:52:53 +0700

    ssh/usernew.sh

--- ssh/usernew.sh
+++ ssh/usernew.sh
@@ -34,12 +34,12 @@ wsssl=`cat /root/log-install.txt | grep -w "SSH SSL Websocket" | cut -d: -f2 | a
           # CREATE S S H 
 green_background="\033[42;37m"
 red_background="\033[41;37m"
+
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\\E[40;1;37m            Add SSH Account      \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
 clear
-
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\\E[40;1;37m      Add SSH Account      \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-
 read -p " Username : " Login
 read -p " Password : " Pass
 read -p " Expired  : " masaaktif
